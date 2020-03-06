c249a3d7b8e627b63d0676459fa731892d425c65
Theo <tzeribi@gmail.com>
2020-03-06 03:36:28 +0100

    %S Finish

--- Printf/main.c
+++ Printf/main.c
@@ -6,7 +6,7 @@
 /*   By: thzeribi <thzeribi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 00:00:53 by thzeribi          #+#    #+#             */
-/*   Updated: 2020/03/05 05:19:17 by thzeribi         ###   ########.fr       */
+/*   Updated: 2020/03/06 03:32:50 by thzeribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,113 @@
 
 int	main(void)
 {
-	//ft_printf("Hello World %.c %d!\n", 10, 10);
-	//ft_printf("Hello %s !\n", "World");
-	char *str = "slt sv ?";
+	/*printf("-------NEGATIFS-------\n\n");
+	printf("-------TEST 1-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%010.d|)", -10));
+	printf(" = vrai = [%d]\n", printf("(|%010.d|)", -10));
+	printf("-------TEST 2-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%00.d|)", -10));
+	printf(" = vrai = [%d]\n", printf("(|%00.d|)", -10));
+	printf("-------TEST 3-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%010.2d|)", -10));
+	printf(" = vrai = [%d]\n", printf("(|%010.2d|)", -10));
+	printf("-------TEST 4-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%00d|)", -10));
+	printf(" = vrai = [%d]\n", printf("(|%00d|)", -10));
+	printf("-------TEST 5-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*.*d|)",10, -3, -10));
+	printf(" = vrai = [%d]\n", printf("(|%0*.*d|)", 10, -3, -10));
+	printf("-------TEST 6-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*.*d|)",-5, 8, -10));
+	printf(" = vrai = [%d]\n", printf("(|%0*.*d|)", -5, 8, -10));
+	printf("-------TEST 7-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%d|)", -10));
+	printf(" = vrai = [%d]\n", printf("(|%d|)", -10));
+	printf("-------TEST 8-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*d|)", -4, -12));
+	printf(" = vrai = [%d]\n", printf("(|%0*d|)", -4, -12));
+	printf("-------TEST 8-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*d|)", 7, -12));
+	printf(" = vrai = [%d]\n", printf("(|%0*d|)", 7, -12));
+	printf("-------TEST 8-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%.*d|)", 7, -12));
+	printf(" = vrai = [%d]\n", printf("(|%.*d|)", 7, -12));
+
+	printf("\n\n\n-------POSITIFS-------\n\n");
+	printf("-------TEST 1-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%010.d|)", 10));
+	printf(" = vrai = [%d]\n", printf("(|%010.d|)", 10));
+	printf("-------TEST 2-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%00.d|)", 10));
+	printf(" = vrai = [%d]\n", printf("(|%00.d|)", 10));
+	printf("-------TEST 3-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%010.2d|)", 10));
+	printf(" = vrai = [%d]\n", printf("(|%010.2d|)", 10));
+	printf("-------TEST 4-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%00d|)", 10));
+	printf(" = vrai = [%d]\n", printf("(|%00d|)", 10));
+	printf("-------TEST 5-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*.*d|)",-10, 3, 10));
+	printf(" = vrai = [%d]\n", printf("(|%0*.*d|)", -10, 3, 10));
+	printf("-------TEST 6-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*.*d|)",-5, 8, 10));
+	printf(" = vrai = [%d]\n", printf("(|%0*.*d|)", -5, 8, 10));
+	printf("-------TEST 7-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%d|)", 10));
+	printf(" = vrai = [%d]\n", printf("(|%d|)", 10));
+	printf("-------TEST 8-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*d|)", -10, 10));
+	printf(" = vrai = [%d]\n", printf("(|%0*d|)", -10, 10));
+
+	printf("\n\n\n-----------ZERO-----------\n\n");
+	printf("-------TEST 1-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%010.d|)", 0));
+	printf(" = vrai = [%d]\n", printf("(|%010.d|)", 0));
+	printf("-------TEST 2-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%00.d|)", 0));
+	printf(" = vrai = [%d]\n", printf("(|%00.d|)", 0));
+	printf("-------TEST 3-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%010.2d|)", 0));
+	printf(" = vrai = [%d]\n", printf("(|%010.2d|)", 0));
+	printf("-------TEST 4-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%00d|)", 0));
+	printf(" = vrai = [%d]\n", printf("(|%00d|)", 0));
+	printf("-------TEST 5-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*.*d|)",-10, 3, 0));
+	printf(" = vrai = [%d]\n", printf("(|%0*.*d|)", -10, 3, 0));
+	printf("-------TEST 6-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*.*d|)",-5, 8, 0));
+	printf(" = vrai = [%d]\n", printf("(|%0*.*d|)", -5, 8, 0));
+	printf("-------TEST 7-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%d|)", 0));
+	printf(" = vrai = [%d]\n", printf("(|%d|)", 0));
+	printf("-------TEST 8-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*d|)", -10, 0));
+	printf(" = vrai = [%d]\n", printf("(|%0*d|)", -10, 0));
+	printf("-------TEST 9-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%d|)", 0));
+	printf(" = vrai = [%d]\n", printf("(|%d|)", 0));
+	printf("-------TEST 10------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%1d|)"));
+
+	printf("\n\n\n-----------CHAR-----------\n\n");
+	printf("-------TEST 1-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%10c|)", '\0'));
+	printf(" = vrai = [%d]\n", printf("(|%10c|)", '\0'));
+	printf("-------TEST 2-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%0*%|)", 10));
+	printf(" = vrai = [%d]\n", printf("(|%0*%|)", 10));
+	printf("-------TEST 3-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%*c|)", 10, 'j'));
+	printf(" = vrai = [%d]\n", printf("(|%*c|)", 10, 'j'));
+	printf("-------TEST 4-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%*c|)", -10, 'j'));
+	printf(" = vrai = [%d]\n", printf("(|%*c|)", -10, 'j'));
+	printf("-------TEST 5-------\n");
+	printf(" = mien = [%d]\n", ft_printf("(|%c|)", '5'));
+	printf(" = vrai = [%d]\n", printf("(|%c|)", '5'));*/
+
+	char *str = "slt";
 	printf("\n\n\n-------STRING-------\n\n");
 	printf("-------TEST 1-------\n");
 	printf(" = mien = [%d]\n", ft_printf("(|%10.s|)", str));
@@ -46,7 +150,10 @@ int	main(void)
 	printf(" = mien = [%d]\n", ft_printf("(|%s|)", str));
 	printf(" = vrai = [%d]\n", printf("(|%s|)", str));
 
-	//system("leaks test");
+	/*printf("%%\n");
+	ft_printf("%%\n");*/
+
+	system("leaks test");
 
 	return (0);
 }
--- Printf/master/srcs/parsing.c
+++ Printf/master/srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: thzeribi <thzeribi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 09:51:33 by thzeribi          #+#    #+#             */
-/*   Updated: 2020/03/05 05:12:08 by thzeribi         ###   ########.fr       */
+/*   Updated: 2020/03/06 03:02:53 by thzeribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,21 +40,21 @@ t_tab	*parse_precisions(t_tab *tab)
 	while (tab->s_parse[tab->i] == '.')
 	{
 		tab->i++;
-		tab->precisions = 0;
+		tab->precisions = -1;
 	}
 	while (ft_isdigit(tab->s_parse[tab->i]) == 1)
 	{
+		tab->precisions = 0;
 		tab->precisions *= 10;
 		tab->precisions += (tab->s_parse[tab->i] - 48);
 		tab->i++;
 	}
-	//printf("\nTAB->COMBIN : %c\n", tab->combin[3]);
-	//printf("\nTAB->ARGS = %d\n",va_arg(tab->args, int));
-	if (tab->combin[3] == '*')
+	if (tab->s_parse[tab->i] == '*')
 	{
 		tab->precisions = va_arg(tab->args, int);
 		if (tab->precisions < 0)
 			tab->precisions = -tab->precisions;
+		tab->i++;
 	}
 	return (tab);
 }
@@ -67,14 +67,6 @@ t_tab	*parse_width(t_tab *tab)
 		tab->width *= 10;
 		tab->width += (tab->s_parse[tab->i] - 48);
 		tab->i++;
-	}
-	if (tab->combin[2] == '*')
-	{
-		tmp = va_arg(tab->args, int);
-		if (tmp <= 0)
-			tab->precisions = -1;
-		else
-			tab->precisions = tmp;
 	}
 	if (tab->combin[3] == '*')
 	{
@@ -104,12 +96,7 @@ t_tab	*parse_combin(t_tab *tab)
 			while(tab->s_parse[tab->i] == '0' && tab->i++)
 				tab->combin[1] = '0';
 			while(tab->s_parse[tab->i] == '*' && tab->i++)
-			{
-				if (tab->combin[2] == '*')
-					tab->combin[2] = '*';
-				else
 					tab->combin[3] = '*';
-			}
 			i = 0;
 		}
 		i++;
