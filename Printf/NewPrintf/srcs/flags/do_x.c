69435202ed2383ee9f98efe35d47135662b0db00
Theo <tzeribi@gmail.com>
2020-10-10 03:53:13 +0200

    Fixing %Xx

--- Printf/NewPrintf/srcs/flags/do_x.c
+++ Printf/NewPrintf/srcs/flags/do_x.c
@@ -6,7 +6,7 @@
 /*   By: thzeribi <thzeribi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 06:33:00 by thzeribi          #+#    #+#             */
-/*   Updated: 2020/09/25 08:07:32 by thzeribi         ###   ########.fr       */
+/*   Updated: 2020/10/10 03:45:02 by thzeribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static void	display_x(t_tab *tab, long int nbr, int blank, int upper)
 	int len;
 
 	len = ft_putlnbr_base(nbr, BASE, upper, FALSE);
-	if (tab->width_is_neg == 0 && tab->combin[0] != '-')
+	if (!tab->width_is_neg && tab->combin[0] != '-' && !tab->prec_is_neg)
 	{
 		if (tab->combin[1] == '0' && tab->prec_null)
 			;
@@ -41,13 +41,15 @@ static void	display_x(t_tab *tab, long int nbr, int blank, int upper)
 				display(tab, ' ', tab->width - blank, TRUE);
 		}
 	}
-	if (tab->prec_is_neg == 0 && tab->precisions >= 0)
+	if (!tab->prec_is_neg && tab->precisions >= 0)
 	{
 		if (tab->combin[1] == '0' && tab->precisions == 0)
 			display(tab, '0', tab->width - len, TRUE);
 		else
 			display(tab, '0', tab->precisions - len, TRUE);
 	}
+	else if (tab->precisions >= 0 && tab->prec_is_neg && tab->combin[1] == '0' && !tab->width_is_neg)
+		display(tab, '0', tab->width - len, TRUE);
 	else
 		blank = len;
 	if ((tab->prec_null || !tab->precisions) && nbr == 0 && tab->combin[3] == '.')
@@ -61,8 +63,7 @@ static void	display_x(t_tab *tab, long int nbr, int blank, int upper)
 		display(tab, ' ', tab->width - blank, TRUE);
 	else if (tab->combin[0] == '-')
 	{
-		if (tab->prec_is_neg == 0 && tab->precisions >= 0
-			&& tab->combin[3] == '.')
+		if (!tab->prec_is_neg && tab->precisions >= 0 && tab->combin[3] == '.')
 			display(tab, ' ', tab->width - blank, TRUE);
 		else
 			display(tab, ' ', tab->width - len, TRUE);
@@ -92,11 +93,10 @@ t_tab		*print_x(t_tab *tab, int upper)
 	nbr = (unsigned)(va_arg(tab->args, unsigned int));
 	if ((int)nbr < 0)
 		tab->nbr_is_neg = 1;
-	if (tab->width == 0 && tab->combin[3] == '.' && tab->precisions == 0
-		&& nbr == 0)
+	if (!tab->width && tab->combin[3] == '.' && !tab->precisions && nbr == 0)
 		return (tab);
 	width = ft_putlnbr_base(nbr, BASE, upper, FALSE);
-	blank = (width <= tab->precisions && tab->precisions > 0 && tab->width >= 0)
+	blank = (width <= tab->precisions && tab->precisions && tab->width >= 0)
 		? tab->precisions : width;
 	display_x(tab, nbr, blank, upper);
 	return (tab);
--- Printf/main.c
+++ Printf/main.c
@@ -6,7 +6,7 @@
 /*   By: thzeribi <thzeribi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/16 00:34:47 by thzeribi          #+#    #+#             */
-/*   Updated: 2020/10/09 20:38:30 by thzeribi         ###   ########.fr       */
+/*   Updated: 2020/10/10 03:52:58 by thzeribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -585,30 +585,52 @@ int	main(void)
 	ft_putstr("\n============================================= DEBUGGER =============================================\n");
 	int		a = -4;
 	int		b = 0;
-	// char	c = 'a';
-	// int		d = 2147483647;
-	// int		e = -2147483648;
+	char	c = 'a';
+	int		d = 2147483647;
+	int		e = -2147483648;
 	// int		f = 42;
 	// int		g = 25;
 	// int		h = 4200;
-	// int		i = 8;
-	// int		j = -12;
-	// int		k = 123456789;
-	// int		l = 0;
-	// int		m = -12345678;
+	int		i = 8;
+	int		j = -12;
+	int		k = 123456789;
+	int		l = 0;
+	int		m = -12345678;
 
+	/*a = -1;
+	int count = 378;
+	while(a < 5)
+	{
+		b = -1;
+		while (b < 5)
+		{
+			printf("\n\n\nTest %d [a: %d, b: %d]\n", count, a, b);
+			printf(" = mien = [%d]\n", ft_printf("(|%*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
+			printf(" = vrai = [%d]\n", printf("(|%*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
+			count++;
 
-	a = 19;
-	b = 14;
-	printf(" = mien = [%d]\n", ft_printf("(|%*.*i, %*.*d, %*.*d, %*.*d, %*.*d, %*.*d, %*.*d, %*.*d|)", a, b, -42, a, b, 42, -a, -b, -42, -a, -b, 42, -a, b, -42, -a, b, 42, a, -b, -42, a, -b, 42));
-	printf(" = vrai = [%d]\n", printf("(|%*.*i, %*.*d, %*.*d, %*.*d, %*.*d, %*.*d, %*.*d, %*.*d|)", a, b, -42, a, b, 42, -a, -b, -42, -a, -b, 42, -a, b, -42, -a, b, 42, a, -b, -42, a, -b, 42));
+			printf("\nTest %d [a: %d, b: %d]\n", count, a, b);
+			printf(" = mien = [%d]\n", ft_printf("(|%0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
+			printf(" = vrai = [%d]\n", printf("(|%0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X, %0*.*u, %0*.*x, %0*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
+			count++;
 
-	printf("\n\n\n\n");
-	printf(" = mien = [%d]\n", ft_printf("(|%*.*d|)", -a, b, -42));
-	printf(" = vrai = [%d]\n", printf("(|%*.*d|)", -a, b, -42));
-	printf(" = mien = [%d]\n", ft_printf("(|%*.*d|)", a, -b, -42));
-	printf(" = vrai = [%d]\n", printf("(|%*.*d|)", a, -b, -42));
+			printf("\nTest %d [a: %d, b: %d]\n", count, a, b);
+			printf(" = mien = [%d]\n", ft_printf("(|%-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
+			printf(" = vrai = [%d]\n", printf("(|%-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X, %-*.*u, %-*.*x, %-*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
+			count++;
 
+			b++;
+		}
+		a++;
+	}*/
+
+	a = 2;
+	b = -1;
+	printf(" = mien = [%d]\n", ft_printf("(|%*.*x|)", 2, -1, 8));
+	printf(" = vrai = [%d]\n", printf("(|%*.*x|)", 2, -1, 8));
+
+	printf(" = mien = [%d]\n", ft_printf("(|%*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
+	printf(" = vrai = [%d]\n", printf("(|%*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X, %*.*u, %*.*x, %*.*X|)", a, b, i, a, b, i, a, b, i, a, b, j, a, b, j, a, b, j, a, b, k, a, b, k, a, b, k, a, b, l, a, b, l, a, b, l, a, b, m, a, b, m, a, b, m, a, b, c, a, b, c, a, b, c, a, b, d, a, b, d, a, b, d, a, b, e, a, b, e, a, b, e));
 
 	ft_putstr("====================================================================================================\n");
 
