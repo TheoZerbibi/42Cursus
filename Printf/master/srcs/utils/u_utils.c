8f4c94ef7049ffeb029704d23f944f0e9ea55a49
Theo <tzeribi@gmail.com>
2020-10-16 17:16:49 +0200

    Norm %u (Not finish)

--- Printf/master/srcs/utils/u_utils.c
+++ Printf/master/srcs/utils/u_utils.c
@@ -6,31 +6,15 @@
 /*   By: thzeribi <thzeribi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/18 03:23:48 by thzeribi          #+#    #+#             */
-/*   Updated: 2020/09/22 06:39:46 by thzeribi         ###   ########.fr       */
+/*   Updated: 2020/10/16 17:15:52 by thzeribi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_printf.h"
 
-void	u_utils_positive(t_tab *tab, int width, int blank)
+static void	u_utils_positive_print_before(t_tab *tab, int width, long int nbr,
+int already_print)
 {
-	if (!tab->width_is_neg && tab->combin[0] != '-')
-	{
-		if (tab->combin[1] == '0' && (tab->combin[3] != '.' || tab->prec_is_neg))
-			return ;
-			if (tab->prec_is_neg)
-				display(tab, ' ', tab->width - width, TRUE);
-			else
-				display(tab, ' ', tab->width - blank, TRUE);
-	}
-}
-
-void	u_utils_positive_print(t_tab *tab, long int nbr,
-int width, int blank)
-{
-	int already_print;
-
-	already_print = 0;
 	if (tab->combin[1] == '0' && tab->combin[0] != '-')
 	{
 		if (tab->combin[3] == '.' && !tab->prec_is_neg && (int)nbr >= 0)
@@ -39,13 +23,14 @@ int width, int blank)
 			display(tab, '0', tab->precisions - width, TRUE);
 		}
 		else if (tab->combin[3] == '.' && tab->combin[2] == '*'
-			&& tab->prec_is_neg && !tab->width_is_neg)
-			{
-				already_print = 1;
-				display(tab, '0', tab->width - width, TRUE);
-			}
+		&& tab->prec_is_neg && !tab->width_is_neg)
+		{
+			already_print = 1;
+			display(tab, '0', tab->width - width, TRUE);
+		}
 	}
-	if (!already_print && tab->combin[3] != '.' && tab->combin[0] != '-' && !tab->width_is_neg)
+	if (!already_print && tab->combin[3] != '.' && tab->combin[0] != '-'
+	&& !tab->width_is_neg)
 	{
 		if (tab->prec_null && tab->combin[1] != '0')
 			;
@@ -54,16 +39,41 @@ int width, int blank)
 	}
 	else if (!already_print && !tab->prec_is_neg)
 		display(tab, '0', tab->precisions - width, TRUE);
-	else if (!already_print && tab->prec_is_neg && tab->combin[1] == '0' && !tab->width_is_neg)
+	else if (!already_print && tab->prec_is_neg && tab->combin[1] == '0'
+	&& !tab->width_is_neg)
 		display(tab, '0', tab->width - width, TRUE);
-	if (((tab->precisions == 1 && tab->prec_null) && tab->combin[3] == '.' && nbr == 0) || (!tab->precisions && nbr == 0 && tab->combin[3] == '.'))
+}
+
+void		u_utils_positive(t_tab *tab, int width, int blank)
+{
+	if (!tab->width_is_neg && tab->combin[0] != '-')
+	{
+		if (tab->combin[1] == '0' && (tab->combin[3] != '.'
+		|| tab->prec_is_neg))
+			return ;
+		if (tab->prec_is_neg)
+			display(tab, ' ', tab->width - width, TRUE);
+		else
+			display(tab, ' ', tab->width - blank, TRUE);
+	}
+}
+
+void		u_utils_positive_print(t_tab *tab, long int nbr,
+int width, int blank)
+{
+	int already_print;
+
+	already_print = 0;
+	u_utils_positive_print_before(tab, width, nbr, already_print);
+	if (((tab->precisions == 1 && tab->prec_null) && tab->combin[3] == '.'
+	&& nbr == 0) || (!tab->precisions && nbr == 0 && tab->combin[3] == '.'))
 	{
 		if (tab->width > 0)
 			display(tab, ' ', 1, TRUE);
 	}
 	else
 	{
-			tab->len += ft_nbrlen(nbr);
+		tab->len += ft_nbrlen(nbr);
 		if (nbr != (-2147483647 - 1))
 			ft_putnbrmax_fd(nbr, 1);
 		else if ((tab->len += 9) > 0)
